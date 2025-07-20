4854699bfb2d5d5fc0ee4eb041bfa6f16007041f
KrakeHyrde <nicolasagustindibuono@gmail.com>
2025-07-20 00:32:23 -0300

    add: seccion de info basica (solo texto)

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,6 +1,17 @@
+import '../styles/Info.css'
 const Info = () => {
     return (
-    <h1>Hola soy la Info</h1>
+        <div className='info'>
+            <div className='texto'>
+                <h1 className='titulo'>Sobre Nosotros</h1>
+                <div className='parrafo'>
+                <p>Somos una Empresa Indie de videojuegos orientados en juegos de Terror Emocional</p>
+                <p>Tenemos como objetivo poder llegar al jugador a traves de historias profundas y terrorificas</p>
+                <p>con distintos tipos de arte pero con un unico objetivo</p>
+                <p>ofrecer calidad al jugador</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
