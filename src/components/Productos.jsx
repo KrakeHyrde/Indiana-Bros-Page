6cf83a888853fa8afc4d3ddac797e96107d8b38b
KrakeHyrde <nicolasagustindibuono@gmail.com>
2025-07-20 23:10:17 -0300

    add: añadido la carta de producto extraido de Bootstrap

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,7 +1,36 @@
-const Productos = () => {
-    return (
-    <h1> Hola soy un producto comprame</h1>
-    )
-}
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
+import '../styles/productos.css'
 
+      /*<Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Placeholder as={Card.Title} animation="glow">
+            <Placeholder xs={6} />
+          </Placeholder>
+          <Placeholder as={Card.Text} animation="glow">
+            <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+            <Placeholder xs={6} /> <Placeholder xs={8} />
+          </Placeholder>
+          <Placeholder.Button variant="primary" xs={6} />
+        </Card.Body>
+      </Card>*/
+function Productos() {
+  return (
+    <div className="cart">
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+}
 export {Productos}
