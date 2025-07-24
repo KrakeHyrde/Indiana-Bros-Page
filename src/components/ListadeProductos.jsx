8460604c5dda14b4094e9f8710a8eb0e88b48477
KrakeHyrde <nicolasagustindibuono@gmail.com>
2025-07-23 21:27:46 -0300

    update: mejora de estilo de produtos e implementacion de texto

--- src/components/ListadeProductos.jsx
+++ src/components/ListadeProductos.jsx
@@ -2,17 +2,24 @@ import { Producto } from "./Producto";
 import tommy from '../assets/Tommy.png';
 import poe from '../assets/Poe.png';
 import pillars from '../assets/Pillars.png';
-import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
+import '../styles/lista.css';
 
 const ListadeProductos = () => {
     return (
-        <CardGroup>
-            <Producto className= 'tommy' img = {tommy} title= "Tommy's Adventure" text =""/>
-            <Producto className= 'poe' img= {poe} title="Midnight Tales: Echoes of Poe" text=""/>
-            <Producto className= 'pillars' img={pillars} title="Elemental Pillars" text=""/>
-        </CardGroup>
+        <div className="productos-wrapper">
+            <CardGroup className="lista-productos">
+                <Producto className='tommy' img={tommy} title="Tommy's Adventure" text="Controla a Tommy, un joven atrapado en un bucle de habitaciones lineales que cambian
+                    sutilmente cada vez que intenta escapar.
+                    Para avanzar y reencontrarse con sus amigos, deberá resolver puzles cada vez más
+                    desafiantes, mientras el entorno se vuelve más extraño e inquietante.
+                    Rompe el bucle, resuelve los enigmas y enfrentá la verdad que ha estado evitando: el origen
+                    de las sombras que lo rodean." />
+                <Producto className='poe' img={poe} title="Midnight Tales: Echoes of Poe" text="Una antología de terror interactiva donde cada episodio reinventa un poema o relato de Edgar Allan Poe en una experiencia narrativa única. El jugador explora escenarios oscuros y decadentes, resolviendo acertijos simbólicos y tomando decisiones que afectan la atmósfera y el desenlace de cada historia. Cada episodio mezcla estética gótica, misterio psicológico y terror poético, transportando al jugador a un mundo donde la locura, la obsesión y la muerte susurran en cada rincón." />
+                <Producto className='pillars' img={pillars} title="Elemental Pillars" text="La magia elemental, antaño fuente de equilibrio, es ahora una fuerza peligrosa y corrupta. Como Sualk, el último guardián de la escuela Krita, el jugador debe usar pilares de fuego, agua, viento y tierra para frenar hordas de criaturas invocadas por Mahadeva, el Dios de la Destrucción. Sin embargo, a medida que se desentrañan los secretos de los pilares, se revela una verdad inquietante: la magia que usas para salvar al mundo podría ser la misma que lo condene." />
+            </CardGroup>
+        </div>
     )    
 }
 
-export {ListadeProductos}
+export { ListadeProductos }
--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -2,6 +2,25 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Placeholder from 'react-bootstrap/Placeholder';
 import '../styles/productos.css'
+function Producto(props) {
+  return (
+    <div className="cart">
+      <div className={`cart ${props.className}`}>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img className='imagen' variant="top" src={props.img} alt='Portada de Videojuego'/>
+          <Card.Body>
+            <Card.Title className='title'>{props.title}</Card.Title>
+            <Card.Text className='text'>
+              {props.text}
+            </Card.Text>
+            <Button variant="primary">Buy</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    </div>
+  );
+}
+export {Producto}
 
       /*<Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
@@ -16,20 +35,3 @@ import '../styles/productos.css'
           <Placeholder.Button variant="primary" xs={6} />
         </Card.Body>
       </Card>*/
-function Producto(props) {
-  return (
-    <div className="cart">
-      <Card style={{ width: '18rem' }}>
-        <Card.Img className='imagen' variant="top" src={props.img} alt='Portada de Videojuego'/>
-        <Card.Body>
-          <Card.Title className='title'>{props.title}</Card.Title>
-          <Card.Text className='text'>
-            {props.text}
-          </Card.Text>
-          <Button variant="primary">Buy</Button>
-        </Card.Body>
-      </Card>
-    </div>
-  );
-}
-export {Producto}
