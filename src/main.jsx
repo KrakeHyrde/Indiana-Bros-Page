c1ffad53e57801eba80035e398ad6ce39ac93526
KrakeHyrde <nicolasagustindibuono@gmail.com>
2025-08-13 20:55:44 -0300

    add: añadido de Responsive Design

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
+import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import { Header } from './components/Header.jsx'
 import { RouterApp } from './router/RouterApp.jsx'
-
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/responsive.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>    
